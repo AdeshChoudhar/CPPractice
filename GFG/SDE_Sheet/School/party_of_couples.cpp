d747743c072ccd81c3142382d8461b5ab3a4496d
Adesh Choudhar <choudharas19.comp@coep.ac.in>
2022-08-22 00:30:12 +0530

    GFG: SDE Sheet - School

---
+++ GFG/SDE_Sheet/School/party_of_couples.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    int findSingle(int N, int arr[])
+    {
+        int ans = 0;
+        for (int i = 0; i < N; i++)
+        {
+            ans ^= arr[i];
+        }
+        return ans;
+    }
+};
